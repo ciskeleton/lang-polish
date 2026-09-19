04e63a3e41fd8458821b41460f0cc7fac2690a83
Kader Bouyakoub <4732702+bkader@users.noreply.github.com>
2026-09-19 19:52:44 +0100

    Initial Commit!

---
+++ src/ci3/pagination_lang.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang['pagination_first_link'] = '&lsaquo; Pierwszy';
+$lang['pagination_last_link'] = 'Ostatni &rsaquo;';
+$lang['pagination_next_link'] = '&gt;';
+$lang['pagination_prev_link'] = '&lt;';
+$lang['pagination_summary_text'] = 'Wyświetlanie od %d do %d z %d wpisów';
